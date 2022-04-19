4e0391a8d2711478170a028e7fff1225d4576d4f
dhifoaksa <dhifo150301@gmail.com>
2022-04-20 02:46:58 +0700

    Ini adalah pesan commit pertama

---
+++ app/src/main/java/com/janug/a10119195latihan1/MainActivity.java
@@ -0,0 +1,16 @@
+package com.janug.a10119195latihan1;
+//20 April
+//10119195-FajarNugraha-IF5
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
